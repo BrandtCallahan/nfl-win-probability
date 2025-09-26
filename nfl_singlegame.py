811ec5ddd3f2978802d5d9360b7b0359b2cdb4fe
Brandt Callahan <74641699+BrandtCallahan@users.noreply.github.com>
2025-09-26 14:20:29 -0500

    Update nfl_singlegame.py

--- nfl_singlegame.py
+++ nfl_singlegame.py
@@ -21,20 +21,6 @@ home_tm = "HOU"
 # model type
 model_type = "Model"
 
-"""
-POSSESSION SIMULATION DOESN'T EXIST FOR NFL YET
-
-if model_type == "Simulation":
-    # number of simulation iterations
-    n = 301
-    # neutral site game
-    neutral_gm = False  # NCAA Tournament
-
-    # single game results (win probability and average point spread)
-    sg_win = game_sim(season, away_tm, home_tm, today, neutral_gm, n)
-
-el
-"""
 if model_type == "Model":
     model = single_game_model(
         data_seasons=season_years,
@@ -46,4 +32,5 @@ if model_type == "Model":
 
 # donut chart for single game
 sim_donut_graph(season, away_tm, home_tm, sg_win, hm_tm_prim=True, aw_tm_prim=True)
+
 
