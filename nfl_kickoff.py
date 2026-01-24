553c8f82a57bee973bea5c3dba8b3926f4a5fe9c
BrandtCallahan <callahanbrandtk@gmail.com>
2026-01-24 11:42:33 -0600

    update main page

--- nfl_kickoff.py
+++ nfl_kickoff.py
@@ -39,10 +39,6 @@ def run_model(
     num_seasons: int,
     visualize: bool,
 ):
-
-    nfl_df = pd.DataFrame()
-    nfl_model_stats = pd.DataFrame()
-
     # data season
     season_years = [season]
     for i in range(num_seasons):
@@ -66,122 +62,98 @@ def run_model(
     week_df = nfl_fastr[nfl_fastr["week"] == week].reset_index(drop=True)
 
     wk_matchup = []
+    today = week_df["gameday"].min()
     for i in range(len(week_df.index)):
         wk_matchup += [
             [
                 week_df.iloc[i]["away_team"],
                 week_df.iloc[i]["home_team"],
-                week_df.iloc[i]["gameday"],
             ]
         ]
 
-    if team_focus.upper() == "ALL":
-        matchup = wk_matchup
-    else:
+    if team_focus.upper() != "ALL":
         for gm in wk_matchup:
             if team_focus in gm:
-                matchup = [gm]
+                wk_matchup = [gm]
 
-    for n, gm in enumerate(matchup):
-        # matchup
-        away_tm = gm[0]
-        home_tm = gm[1]
-        today = gm[2]
+    # run the model
+    model = single_game_model(
+        data_seasons=season_years,
+        today=today,
+        week=week,
+        matchups=wk_matchup,
+    )
 
-        logger.info(f"Running {n+1}/{len(matchup)}: {away_tm} vs. {home_tm}")
-
-        # model type
-        model = single_game_model(
-            data_seasons=season_years,
-            today=today,
-            matchup=f"{away_tm} vs. {home_tm}",
-        )
-        sg_win = model[3]
-
-        # reformat df
-        if sg_win["Win Prob."][0] > sg_win["Win Prob."][1]:
-            # Away Tm win
-            tmp_df = pd.DataFrame(
-                data={
-                    "Matchup": [f"{away_tm} vs. {home_tm}"],
-                    "Away Pts": [sg_win["Pred. Pts"][0]],
-                    "Home Pts": [sg_win["Pred. Pts"][1]],
-                    "Total Pts": [sg_win["Pred. Pts"][0] + sg_win["Pred. Pts"][1]],
-                    "Pt Diff": [abs(sg_win["Point Diff"][0])],
-                    "Pred. W": [f"{away_tm}"],
-                    "Win Prob.": [sg_win["Win Prob."][0]],
-                    "Away Moneyline": [sg_win["M/L"][0]],
-                    "Home Moneyline": [sg_win["M/L"][1]],
-                    "Home Spread": [sg_win["Spread"][1]],
-                }
-            )
-            if sg_win["Spread W"][0] > sg_win["Spread W"][1]:
-                tmp_df["Pred. Spread W"] = f"{away_tm}"
-                tmp_df["Spread W Prob."] = f"{round(sg_win['Spread W'][0], 3)}"
-            else:
-                tmp_df["Pred. Spread W"] = f"{home_tm}"
-                tmp_df["Spread W Prob."] = f"{round(sg_win['Spread W'][1], 3)}"
+    if (visualize) & (team_focus != "ALL"):
+        # ensure Pt Diff reflects Hm value properly
+        if model[0]["Home W"][0] == 0:
+            if model[0]["Home Pt Diff"][0] > 0:
+                model[0]["Home Pt Diff"][0] = model[0]["Home Pt Diff"][0] * -1
         else:
-            # Home Tm win
-            tmp_df = pd.DataFrame(
-                data={
-                    "Matchup": [f"{away_tm} vs. {home_tm}"],
-                    "Away Pts": [sg_win["Pred. Pts"][0]],
-                    "Home Pts": [sg_win["Pred. Pts"][1]],
-                    "Total Pts": [sg_win["Pred. Pts"][0] + sg_win["Pred. Pts"][1]],
-                    "Pt Diff": [abs(sg_win["Point Diff"][1])],
-                    "Pred. W": [f"{home_tm}"],
-                    "Win Prob.": [sg_win["Win Prob."][1]],
-                    "Away Moneyline": [sg_win["M/L"][0]],
-                    "Home Moneyline": [sg_win["M/L"][1]],
-                    "Home Spread": [sg_win["Spread"][1]],
-                }
-            )
-            if sg_win["Spread W"][0] > sg_win["Spread W"][1]:
-                tmp_df["Pred. Spread W"] = f"{away_tm}"
-                tmp_df["Spread W Prob."] = f"{round(sg_win['Spread W'][0], 3)}"
-            else:
-                tmp_df["Pred. Spread W"] = f"{home_tm}"
-                tmp_df["Spread W Prob."] = f"{round(sg_win['Spread W'][1], 3)}"
+            if model[0]["Home Pt Diff"][0] < 0:
+                model[0]["Home Pt Diff"][0] = model[0]["Home Pt Diff"][0] * -1
 
-        model_stats = model[1]
+        # Tm Scores
+        if model[0]["Home Pt Diff"][0] < 0:
+            pts = (model[0]['Total Pts'][0]) + (model[0]['Home Pt Diff'][0])
 
-        # reformat model stats
-        tmp_stats = pd.DataFrame(
+            aw_points = pts / 2
+            hm_points = (pts / 2) + (model[0]['Home Pt Diff'][0])
+        else:
+            pts = (model[0]["Total Pts"][0]) - (model[0]["Home Pt Diff"][0])
+
+            aw_points = pts / 2
+            hm_points = (pts / 2) + (model[0]["Home Pt Diff"][0])
+
+        # format a df to fit the donut chart
+        sg_win = pd.DataFrame(
             data={
-                "Matchup": [f"{away_tm} vs. {home_tm}"],
-                "W Exp. Accuracy": [round(model_stats["F1 Score"][0], 3)],
-                "Spread Exp. Accuracy": [round(model_stats["F1 Score"][1], 3)],
-                "Pt Diff Accuracy": [round(model_stats["R^2"][2], 3)],
-                "Home Pts Accuracy": [round(model_stats["R^2"][3], 3)],
-                "Home Pts +/-": [round(model_stats["RMSE"][3], 2)],
+                "Tm": [
+                    model[0]["Away Team"][0],
+                    model[0]["Home Team"][0],
+                ],
+                "Records": [
+                    model[0]["Away Record"][0],
+                    model[0]["Home Record"][0],
+                ],
+                "Win Prob.": [
+                    1 - model[0]["Home W Probability"][0],
+                    model[0]["Home W Probability"][0],
+                ],
+                "Point Diff": [
+                    model[0]["Home Pt Diff"][0] * -1,
+                    model[0]["Home Pt Diff"][0],
+                ],
+                "Pred. Pts": [
+                    round(aw_points, 0),
+                    round(hm_points, 0),
+                ],
+                "Spread W": [
+                    1 - model[0]["Home Spread W Probability"][0],
+                    model[0]["Home Spread W Probability"][0],
+                ],
+                "M/L": [
+                    model[0]["Away Moneyline"][0],
+                    model[0]["Home Moneyline"][0],
+                ],
+                "Spread": [
+                    model[0]["Home Spread"][0] * -1,
+                    model[0]["Home Spread"][0],
+                ],
             }
         )
 
-        if visualize:
-            # donut chart for single game
-            sim_donut_graph(
-                season=season,
-                away_tm=away_tm,
-                home_tm=home_tm,
-                sim_results_df=sg_win,
-                hm_tm_prim=True,
-                aw_tm_prim=True,
-            )
+        # donut chart for single game
+        sim_donut_graph(
+            season=season,
+            away_tm=sg_win["Tm"][0],
+            home_tm=sg_win["Tm"][1],
+            sim_results_df=sg_win,
+            hm_tm_prim=True,
+            aw_tm_prim=True,
+        )
 
-        if nfl_df.empty:
-            nfl_df = tmp_df.copy()
-        else:
-            nfl_df = pd.concat([nfl_df, tmp_df]).reset_index(drop=True)
-
-        if nfl_model_stats.empty:
-            nfl_model_stats = tmp_stats.copy()
-        else:
-            nfl_model_stats = pd.concat([nfl_model_stats, tmp_stats]).reset_index(
-                drop=True
-            )
-
-    return [nfl_df, nfl_model_stats]
+    return [model[0], model[1]]
 
 
 def run_simulation(
@@ -343,7 +315,7 @@ refresh_data(2025)
 """
 nfl_df = run_model(
     season=2025,
-    week=16,
+    week=18,
     team_focus="TEN",
     num_seasons=10,
     visualize=True,
